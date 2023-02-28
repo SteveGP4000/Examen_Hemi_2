c949d88faa13d59354659ebbd545369261ad2c12
NOTSTEVEN4000 <bsgp1986@gmail.com>
2023-02-28 08:32:28 -0500

    listar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/shared/navbar/navbar.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { FormComponent } from './componentes/form/form.component';
+import { ListarComponent } from './componentes/listar/listar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    FormComponent
+    FormComponent,
+    ListarComponent
   ],
   imports: [
     BrowserModule,
