0a09b9ba01bb205f038da3d5965792a95bcc0411
Cristhian-18 <cordovacristhian32@gmail.com>
2023-02-28 08:10:37 -0500

    registro

--- src/app/service/callcenter.service.ts
+++ src/app/service/callcenter.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class CallcenterService {
 
-  constructor() { }
+  constructor() { 
+  }
+  Registro:Registro []=[{
+      codigo_registro:1,
+      fecha:'2023-12-31',
+      cliente:'Cliente',
+      tipo_iteracion:'0984332923',
+      descripcion:'sss'
+  }]
+}
+export interface Registro {
+  codigo_registro:number,
+  fecha:string,
+  cliente:string,
+  tipo_iteracion:string,
+  descripcion:string
 }
