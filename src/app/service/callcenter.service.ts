730b9c8f11002beef03ab8e7a0be74c867c9daca
jeinermenxd <mjair360@gmail.com>
2023-02-28 08:18:54 -0500

    servicio

--- src/app/service/callcenter.service.ts
+++ src/app/service/callcenter.service.ts
@@ -12,8 +12,81 @@ export class CallcenterService {
       fecha:'2023-12-31',
       cliente:'Cliente',
       tipo_iteracion:'0984332923',
-      descripcion:'sss'
+      descripcion:'Servicio'
+  },
+  {
+   
+      codigo_registro:2,
+      fecha:'2022-02-15',
+      cliente:'Cliente',
+      tipo_iteracion:'0978541236',
+      descripcion:'Compra'
+  },
+  {
+   
+      codigo_registro:3,
+      fecha:'2022-04-19',
+      cliente:'Cliente',
+      tipo_iteracion:'0987536482',
+      descripcion:'Servicio'
+  },
+  {
+   
+      codigo_registro:4,
+      fecha:'2023-03-15',
+      cliente:'Cliente',
+      tipo_iteracion:'0951234683',
+      descripcion:'Compra'
+  },
+  {
+   
+      codigo_registro:5,
+      fecha:'2022-12-25',
+      cliente:'Cliente',
+      tipo_iteracion:'0978452182',
+      descripcion:'Servicio'
+  },
+  {
+   
+      codigo_registro:6,
+      fecha:'2022-11-30',
+      cliente:'Cliente',
+      tipo_iteracion:'0978563297',
+      descripcion:'Compra'
+  },
+  {
+   
+      codigo_registro:7,
+      fecha:'2023-01-15',
+      cliente:'Cliente',
+      tipo_iteracion:'0963215689',
+      descripcion:'Servicio'
+  },
+  {
+   
+      codigo_registro:8,
+      fecha:'2022-06-28',
+      cliente:'Cliente',
+      tipo_iteracion:'0987563569',
+      descripcion:'Compra'
+  },
+  {
+   
+      codigo_registro:9,
+      fecha:'2023-02-06',
+      cliente:'Cliente',
+      tipo_iteracion:'0987456328',
+      descripcion:'Servicio'
+  },
+  {
+   
+      codigo_registro:10,
+      fecha:'2022-03-26',
+      cliente:'Cliente',
+      tipo_iteracion:'0951598756',
+      descripcion:'Compra'
   }]
+  
 }
 export interface Registro {
   codigo_registro:number,
